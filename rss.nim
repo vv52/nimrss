85d8d55cb2d6a8987484944f7183460979324a16
Vanja Venezia <vanjavenezia@gmail.com>
2025-08-12 01:12:18 -0700

    basic proof of concept with parsing atom feed by title and date

--- rss.nim
+++ rss.nim
@@ -1,10 +1,41 @@
-import std/httpclient, xmlparser, xmltree
+import httpClient, streams
+import xmlparser, xmltree
+import times
 
-var client = newHttpClient()
-var rss_feed = "https://dateemups.com/feed/"
-try:
-  let x = parseXml(client.getContent(rss_feed))
-  for element in x:
-    echo element
-finally:
+type
+  atomChannel = object
+    id : string
+    title : string
+    updated : DateTime
+    author : string
+    link : string
+
+#   rssChannel = object
+#     title : string
+#     link : string
+#     description : string
+
+proc getXml(): XmlNode =
+  var client = newHttpClient()
+  var rss_feed = "https://dateemups.com/feed/"
+  let xml = client.getContent(rss_feed)
   client.close()
+  let xmlStream = newStringStream(xml)
+  return parseXml(xmlStream)
+
+proc test(): seq[atomChannel] =
+  let xmlRoot = getXml()
+  var posts : seq[atomChannel]
+  for item_node in xmlRoot.findAll("entry"):
+    echo item_node.child("title").innerText
+    echo item_node.child("published").innerText
+    echo item_node.child("id").innerText
+    var post = atomChannel(id: item_node.child("id").innerText, title: item_node.child("title").innerText)
+    posts.add(post)
+  result = posts
+
+proc main =
+  echo test()
+
+when isMainModule:
+  main()
